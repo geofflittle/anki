ddd079a1cc121a4f08cb826b845572e07c6f061b
Geoff Little <g.little712@gmail.com>
2020-11-11 22:06:13 -0600

    Allow nested note sections

--- test_yaml.py
+++ test_yaml.py
@@ -1,6 +1,28 @@
 import yaml
+import re
+
+alpha = re.compile('[^A-Za-z ]+')
+
+
+def get_dash_case(val):
+    return alpha.sub('', val).lower().replace(" ", "-")
+
+
+def get_notes(text):
+    title = get_dash_case(text["title"])
+    notes = [(title + "-" + str(i), note) for (i, note) in enumerate(text["notes"])] if "notes" in text else []
+    section_notes = [(title + "-" + t, note) for section in text["sections"] for (t, note) in get_notes(section)] \
+        if "sections" in text else []
+    return notes + section_notes
+
+
+def run(path):
+    with open(path) as f:
+        text = yaml.load(f, Loader=yaml.FullLoader)
+        notes = get_notes(text)
+        for note in notes:
+            print(note)
+
 
 if __name__ == "__main__":
-    with open("van_sickles_modern_airmanship.yml") as f:
-        notes = yaml.load(f, Loader=yaml.FullLoader)
-        print(notes)
+    run("van_sickles_modern_airmanship.yml")
